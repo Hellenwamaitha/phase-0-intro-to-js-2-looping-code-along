76ce36734e490238a3ab4ef75f12b0ddd5c37b77
hellenwamaitha <hellenwama254@gmail.com>
2023-06-18 14:13:55 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  const names = ["Guadalupe", "Ollie", "Aki"];
+const event = 'birthday';
+
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
